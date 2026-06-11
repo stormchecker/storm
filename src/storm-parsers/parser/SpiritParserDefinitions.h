0a6a2d4d6ae5f3de926c8e9fb62f5f566a6f2b08
Luko van der Maas <luko.vandermaas@ru.nl>
2026-06-11 11:22:32 +0200

    Add precise deprecated ignore

--- src/storm-parsers/parser/SpiritParserDefinitions.h
+++ src/storm-parsers/parser/SpiritParserDefinitions.h
@@ -4,6 +4,11 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-W#pragma-messages"
 
+#if defined(__clang__) && defined(__apple_build_version__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#endif
+
 // Include boost spirit.
 #define BOOST_SPIRIT_USE_PHOENIX_V3
 #define BOOST_SPIRIT_UNICODE
@@ -27,5 +32,9 @@ namespace spirit_encoding = boost::spirit::unicode;
 }
 
 typedef BOOST_TYPEOF(storm::spirit_encoding::space_type() | qi::lit("//") >> *(qi::char_ - (qi::eol | qi::eoi)) >> (qi::eol | qi::eoi)) Skipper;
+
+#if defined(__clang__) && defined(__apple_build_version__)
+#pragma clang diagnostic pop
+#endif
 
 #endif /* STORM_PARSER_SPIRITPARSERDEFINITIONS_H_ */
