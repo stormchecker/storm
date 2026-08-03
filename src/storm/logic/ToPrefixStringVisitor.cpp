7542787f4f5c9053e8d682f615d773c0074ce93a
Matthias Volk <m.volk@tue.nl>
2026-08-03 12:38:35 +0200

    Removed break after return

--- src/storm/logic/ToPrefixStringVisitor.cpp
+++ src/storm/logic/ToPrefixStringVisitor.cpp
@@ -29,10 +29,8 @@ boost::any ToPrefixStringVisitor::visit(BinaryBooleanStateFormula const& f, boos
     switch (f.getOperator()) {
         case BinaryBooleanStateFormula::OperatorType::And:
             return std::string("& ") + left + " " + right;
-            break;
         case BinaryBooleanStateFormula::OperatorType::Or:
             return std::string("| ") + left + " " + right;
-            break;
     }
     return boost::any();
 }
@@ -43,10 +41,8 @@ boost::any ToPrefixStringVisitor::visit(BinaryBooleanPathFormula const& f, boost
     switch (f.getOperator()) {
         case BinaryBooleanPathFormula::OperatorType::And:
             return std::string("& ") + left + " " + right;
-            break;
         case BinaryBooleanPathFormula::OperatorType::Or:
             return std::string("| ") + left + " " + right;
-            break;
     }
     return boost::any();
 }
@@ -169,7 +165,6 @@ boost::any ToPrefixStringVisitor::visit(UnaryBooleanStateFormula const& f, boost
     switch (f.getOperator()) {
         case UnaryBooleanStateFormula::OperatorType::Not:
             return std::string("! ") + subexpression;
-            break;
     }
     return boost::any();
 }
@@ -179,7 +174,6 @@ boost::any ToPrefixStringVisitor::visit(UnaryBooleanPathFormula const& f, boost:
     switch (f.getOperator()) {
         case UnaryBooleanPathFormula::OperatorType::Not:
             return std::string("! ") + subexpression;
-            break;
     }
     return boost::any();
 }
