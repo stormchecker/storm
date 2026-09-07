d9f78df77e6372bb29392913f0242352aa88863f
Victor Voronin <vivo@eiva.com>
2026-09-07 13:03:56 +0200

    Unblock WeakUntil/Release in JaniParser
    and add corresponding visitor code
    
    Signed-off-by: Victor Voronin <vivo@eiva.com>

--- src/storm/logic/ToPrefixStringVisitor.cpp
+++ src/storm/logic/ToPrefixStringVisitor.cpp
@@ -183,6 +183,18 @@ boost::any ToPrefixStringVisitor::visit(UntilFormula const& f, boost::any const&
     return std::string("U ") + left + " " + right;
 }
 
+boost::any ToPrefixStringVisitor::visit(WeakUntilFormula const& f, boost::any const& data) const {
+    std::string left = boost::any_cast<std::string>(f.getLeftSubformula().accept(*this, data));
+    std::string right = boost::any_cast<std::string>(f.getRightSubformula().accept(*this, data));
+    return std::string("W ") + left + " " + right;
+}
+
+boost::any ToPrefixStringVisitor::visit(ReleaseFormula const& f, boost::any const& data) const {
+    std::string left = boost::any_cast<std::string>(f.getLeftSubformula().accept(*this, data));
+    std::string right = boost::any_cast<std::string>(f.getRightSubformula().accept(*this, data));
+    return std::string("R ") + left + " " + right;
+}
+
 boost::any ToPrefixStringVisitor::visit(HOAPathFormula const&, boost::any const&) const {
     STORM_LOG_THROW(false, storm::exceptions::InvalidOperationException, "Can not convert to prefix string.");
 }
